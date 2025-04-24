0f49e2ad8f7ab9419a1a215be125654f69351d55
kannakalyangit <kannakalyan.devops@gmail.com>
2025-04-24 19:15:25 +0530

    Added a new Feature SQRT

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -16,8 +16,8 @@ class Calculator:
 
 # TODO: Implement the following function to calculate the square root of a number.
 
-# def square_root(self, x):
-#     return math.sqrt(x)
+def square_root(self, x):
+    return math.sqrt(x)
 
 # You need to uncomment the above function and complete its implementation to add the square root feature.
 
